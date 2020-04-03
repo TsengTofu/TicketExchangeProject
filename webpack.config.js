56414a9ea3782b098c0ecc4f3bad80d53b091a7a
Tofu <tsengbatty@gmail.com>
2020-04-03 13:04:22 +0800

    [add]Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+// 通常是不需要 但如果架設的環境比較複雜 最好還是建立一個
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+        ],
+    },
+};
